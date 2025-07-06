5880d9030e7785531256e5dfa9732119fb7402b2
Ashot Manukyan <ashot.manukyan@gohacktech.com>
2025-07-06 18:46:25 +0400

    login part

--- apps/auth-app/src/app/components/login/login.component.ts
+++ apps/auth-app/src/app/components/login/login.component.ts
@@ -1,6 +1,11 @@
 // login.component.ts
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -8,8 +13,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-
 import { AuthService } from '../../services/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -21,42 +26,62 @@ import { AuthService } from '../../services/auth.service';
     MatButtonModule,
     MatIconModule,
     MatSnackBarModule,
-    FormsModule,
+    ReactiveFormsModule,
     RouterLink,
+    NgIf,
   ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  login = {
-    email: '',
-    password: '',
-  };
-
-  showPassword = false;
-
+  private fb = inject(FormBuilder);
   private authService = inject(AuthService);
   private router = inject(Router);
   private snackBar = inject(MatSnackBar);
 
+  loginForm: FormGroup = this.fb.group({
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/),
+      ],
+    ],
+    password: ['', Validators.required],
+  });
+
+  hidePassword = true;
+
+  get email() {
+    return this.loginForm.get('email')!;
+  }
+
+  get password() {
+    return this.loginForm.get('password')!;
+  }
+
   onLogin() {
-    const { email, password } = this.login;
-    this.authService.getUserDetails(email, password).subscribe({
-      next: (response) => {
-        if (response.length >= 1) {
-          sessionStorage.setItem('email', email);
-          this.router.navigate(['home']);
-        } else {
+    this.loginForm.markAllAsTouched();
+
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.authService.getUserDetails(email, password).subscribe({
+        next: (response) => {
+          if (response.length >= 1) {
+            sessionStorage.setItem('email', email);
+            this.router.navigate(['home']);
+          } else {
+            this.snackBar.open('Something went wrong', 'Close', {
+              duration: 3000,
+            });
+          }
+        },
+        error: () => {
           this.snackBar.open('Something went wrong', 'Close', {
             duration: 3000,
           });
-        }
-      },
-      error: () => {
-        this.snackBar.open('Something went wrong', 'Close', {
-          duration: 3000,
-        });
-      },
-    });
+        },
+      });
+    }
   }
 }
